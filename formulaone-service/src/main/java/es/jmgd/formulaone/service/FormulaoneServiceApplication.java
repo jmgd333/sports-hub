7bdc6e47645c8fc30c778d62695a6d481e575ee0
jmgd333 <jmgd4java@gmail.com>
2022-08-09 21:12:58 +0200

    Formula One Service. Initial commit

---
+++ formulaone-service/src/main/java/es/jmgd/formulaone/service/FormulaoneServiceApplication.java
@@ -0,0 +1,13 @@
+package es.jmgd.formulaone.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormulaoneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormulaoneServiceApplication.class, args);
+	}
+
+}
---
+++ formulaone-service/src/test/java/es/jmgd/formulaone/service/FormulaoneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jmgd.formulaone.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaoneServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
