14180131b79dcb9bdc568a5baad0971ae561077d
jmgd333 <jmgd4java@gmail.com>
2022-08-16 17:15:36 +0200

    Initial commit of registry-service

---
+++ registry-service/src/test/java/es/jmgd/registry/service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jmgd.registry.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
