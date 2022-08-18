495a6c7e9e643ac8544271bfe5fd878bb9c0dc22
jmgd333 <jmgd4java@gmail.com>
2022-08-18 18:21:31 +0200

    Refactor Feign Clients & configuration import branch

--- football-service/src/main/java/es/jmgd/football/rest/client/InformationClient.java
+++ football-service/src/main/java/es/jmgd/football/rest/client/InformationClient.java
@@ -6,12 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.jmgd.football.rest.model.information.Information;
 
-@FeignClient(name = "info-service")
-@RequestMapping(path = "/information")
+@FeignClient(name = "info-service", path = "/information")
 public interface InformationClient {
 	@PostMapping
 	public ResponseEntity<Void> addRequestInformation(@Valid @RequestBody Information information);
